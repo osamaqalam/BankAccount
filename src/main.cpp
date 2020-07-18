148b8a30985cc72a6a6c62fd0f37cece2c462698
Osama <osama.qalam@hotmail.com>
2020-07-18 19:53:16 -0400

    Implemented the tester

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,24 @@
 
 #include "Account.h"
 #include "SavingsAccount.h"
+#include "CheckingAccount.h"
+
 
 
 int main ()
 {
 	Account acc (1000);
-	SavingsAccount sAcc(1000,0.1);
+	SavingsAccount sAcc(2000,0.1);
+	CheckingAccount cAcc (3000, 5);
 	double interest = sAcc.calculateInterest();
+
+	cout << "Original savings account's balance = " << sAcc.getBalance() << endl;
 	sAcc.credit(interest);
-	cout << sAcc.getBalance() << endl;
+	cout << "Savings account's balance after interest = " << sAcc.getBalance() << "\n\n";
+
+	cout << "Original checking account's balance = " << cAcc.getBalance() << endl;
+	cAcc.debit(500);
+	cout << "Checking account's balance after withdrawal = " << cAcc.getBalance() << endl;
 
 
 }
